6a49589b076fb5327db9a7360bef195f815a9e1b
rifaauliary <105924234+rifaauliary@users.noreply.github.com>
2022-12-20 10:46:41 +0700

    projek pertama kali digunakan di git

---
+++ AppsBangunDatar/src/main/java/com/appsbangundatar/BangunDatar.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.appsbangundatar;
+
+/**
+ *
+ * @author rifa
+ */
+public abstract class BangunDatar {
+    private String namaBangunDatar;
+
+    public String getNamaBangunDatar() {
+        return namaBangunDatar;
+    }
+
+    public void setNamaBangunDatar(String namaBangunDatar) {
+        this.namaBangunDatar = namaBangunDatar;
+    }
+    
+    abstract double luas();
+    abstract double keliling(); 
+}
